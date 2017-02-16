4508d999a8491e1237ced3762cfd5b7a4090f763
wknar <wakana.ara@gmail.com>
2017-02-16 16:43:07 +0900

    Initial Commit

---
+++ sdt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sdt
+//
+//  Created by アラ 若菜 on 2017/02/16.
+//  Copyright © 2017 wknar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
