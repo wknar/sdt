a32c45784007cdcd029c8c62ef6a5fe4fd0c147a
wknar <wakana.ara@gmail.com>
2017-02-23 20:28:07 +0900

    add first view

--- sdt/ViewController.swift
+++ sdt/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var uppLbl:     UILabel?
+    var rowLbl:     UILabel?
+
+    var garbBag =   UIImageView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // set tags in each views
+        self.uppLbl?.tag  = 0
+        self.rowLbl?.tag  = 1
+        self.garbBag.tag = 2
+
+        initView()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func initView() {
+        self.garbBag.image = UIImage(named: "garbageBag3.png")
+        self.garbBag.frame = viewFrames(tag: self.garbBag.tag)
+       self.view.addSubview(self.garbBag)
     }
 
+    func viewFrames(tag: Int) -> CGRect {
+        let screenSize: CGRect = UIScreen.main.bounds
+        switch tag {
+        case 0:
+            break
+        case 1:
+            break
+        case 2:
+            return CGRect(x: screenSize.width/2-50, y: screenSize.height/2-50, width: 100, height: 100)
+        default: return CGRect.zero
+        }
+        return CGRect.zero
+    }
 
 }
 
